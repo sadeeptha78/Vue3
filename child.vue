0f127286bbe5dd5a7c97d2eb31474eaf0bc19c51
sadeeptha78 <63169222+sadeeptha78@users.noreply.github.com>
2025-05-23 06:18:03 +0100

    Pass data from Child to Parent and Parent to Child

---
+++ child.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <div class="card-overlay">
+        <div class="card position">
+            <div class="card-header">
+                <button class="btn btn-success btn-sm ms-auto" @click="Confirmed">Confirmed</button>
+            </div>
+            <div class="card-body">
+                <div class="py-3">
+                    <h1>{{info}}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import {defineEmits} from 'vue'
+defineProps(['info'])
+
+const emit = defineEmits(['SendRespondBack'])
+
+let Confirmed = () => {
+    //Can pass any value
+    let data =["a",1,"b",4]
+    emit('SendRespondBack',data)
+}
+
+
+</script>
+
+<style scoped>
+.card-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1040;
+}
+
+.position {
+    position: fixed;
+    width: 85%;
+    top: 20%;
+    left: 5%;
+    z-index: 1050;
+}
+</style>
---
+++ main.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container my-5">
+        <h1 class="text-center"> Card With Header </h1>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-header">
+                        This is card header
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"> this is card text</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <h2 class="text-center mb-4">Our Services</h2>
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">Development</h5>
+                        <p class="card-text">Test Page </p>
+                        <button class="btn btn-primary" @click="openModal()"> Learn More</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">App Development</h5>
+                        <p class="card-text">Good work</p>
+                        <a href="#" class="btn btn-primary">Discover More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row row-cols-1 mt-5" v-if="modalShow" >
+            <div class="d-flex justify-content-end">
+                <child @SendRespondBack="GetRespondFromChild" :info="infoToChild"/>
+            </div>
+        </div>
+    </div>
+
+    <!--Pass value to child and get the return data back form child-->
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import Child from "@/Pages/TestEnv/child.vue";
+
+let test = ref("True value")
+let modalShow = ref(false)
+
+let openModal = () => {
+    modalShow.value = true
+}
+
+let infoToChild = "data from parent"
+
+function GetRespondFromChild(val){
+    console.log(val)
+    modalShow.value =false
+}
+
+</script>
+
+<style scoped>
+
+</style>
